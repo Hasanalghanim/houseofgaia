4a5b2646a02a85660c49e3b0dabb7bfebae7a0c0
Hasan Al <Hasanalghanim@gmail.com>
2025-04-23 14:34:29 -0600

    Update urls.py

--- houseofgaia/urls.py
+++ houseofgaia/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
