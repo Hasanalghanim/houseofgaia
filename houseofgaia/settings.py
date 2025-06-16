9918de5f736cbc9ac91105937164a6a33e1934c2
Hasan Al <Hasanalghanim@gmail.com>
2025-06-15 23:07:54 -0600

    Update settings.py

--- houseofgaia/settings.py
+++ houseofgaia/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/houseofgaia/houseofgaia/staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/houseofgaia/houseofgaia/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
